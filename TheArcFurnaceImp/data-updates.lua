7fb04a49bfa0d52bc5175ba38a2555b86dccc96f
gabfv <gab.github@gmail.com>
2022-08-12 00:06:49 -0400

    Fixed startup errors. - Refactored the copy/creation of recipe with the use of deep_copy() from stdlib

--- TheArcFurnaceImp/data-updates.lua
+++ TheArcFurnaceImp/data-updates.lua
@@ -1,65 +1,4 @@
-local function copy_recipe_data(src, dst, scale)
-    -- does this recipe have "normal" or "expensive" variants?
-    -- If so, the recipe data is actually in those tables.
-    -- (... if they are tables - they can be false if the recipe is to be disabled in that mode).
-    if src["normal"] ~= nil or src["expensive"] ~= nil then
-        if type(src["normal"]) == "table" then
-            dst["normal"] = {}
-            copy_recipe_data(src["normal"], dst["normal"], scale)
-        else
-            dst["normal"] = src["normal"]
-        end
-    
-        if type(src["expensive"]) == "table" then
-            dst["expensive"] = {}
-            copy_recipe_data(src["expensive"], dst["expensive"], scale)
-        else
-            dst["expensive"] = src["expensive"]
-        end
-    else
-        dst["ingredients"] = {}
-        for idx, ispec in pairs(src["ingredients"]) do
-            -- Ingredient prototypes exist in two flavours: array-like and map-like.
-            if #ispec > 1 then
-                dst["ingredients"][idx] = {
-                    ispec[1],
-                    ispec[2] * scale
-                }
-            else
-                dst["ingredients"][idx] = {
-                    ["type"] = ispec["type"],
-                    ["name"] = ispec["name"],
-                    ["amount"] = ispec["amount"] * scale,
-                    ["catalyst_amount"] = (ispec["catalyst_amount"] or 0) * scale,
-                    ["temperture"] = ispec["temperature"],
-                    ["minimum_temperture"] = ispec["minimum_temperature"],
-                    ["maximum_temperture"] = ispec["maximum_temperature"],
-                    ["fluidbox_index"] = ispec["fluidbox_index"]
-                }
-            end
-        end
-
-        dst["energy_required"] = src["energy_required"]
-
-        if src["result"] ~= nil then
-            dst["result"] = src["result"]
-            dst["result_count"] = (src["result_count"] or 1) * scale
-        end
-
-        if src["results"] ~= nil then
-            dst["results"] = {}
-            for idx, rspec in pairs(src["results"]) do
-                dst["results"][idx] = {
-                    ["type"] = rspec["type"],
-                    ["name"] = rspec["name"],
-                    ["amount"] = rspec["amount"] * scale,
-                    ["temperture"] = rspec["temperature"]
-                }
-            end
-        end
-    end
-end
-
+local utils_table = require('__stdlib__/stdlib/utils/table')
 
 local function mimic_recipe_module_limitations(source_recipe, target_recipe)
     for _, module in pairs(data.raw["module"]) do
@@ -83,19 +22,63 @@ local function mimic_recipe_module_limitations(source_recipe, target_recipe)
     end
 end
 
+local function scale_recipe(recipe, scale)
+    if recipe["normal"] ~= nil or recipe["expensive"] ~= nil then
+        if type(recipe["normal"]) == "table" then
+            scale_recipe(recipe["normal"], scale)
+        end
+    
+        if type(recipe["expensive"]) == "table" then
+            scale_recipe(recipe["expensive"], scale)
+        end
+    else
+        for idx, ispec in pairs(recipe["ingredients"]) do
+            -- Ingredient prototypes exist in two flavours: array-like and map-like.
+            if #ispec > 1 then
+                recipe["ingredients"][idx] = {
+                    ispec[1],
+                    ispec[2] * scale
+                }
+            else
+                recipe["ingredients"][idx]["amount"] = ispec["amount"] * scale
+                if recipe["ingredients"][idx]["catalyst_amount"] ~= nil then
+                    recipe["ingredients"][idx]["catalyst_amount"] = ispec["catalyst_amount"] * scale
+                end
+            end
+        end
+        
+        if recipe["result"] ~= nil then
+            recipe["result_count"] = (recipe["result_count"] or 1) * scale
+        end
+
+        if recipe["results"] ~= nil then
+            for idx, rspec in pairs(recipe["results"]) do
+                if rspec["amount"] ~= nil then
+                    recipe["results"][idx]["amount"] = rspec["amount"] * scale
+                else -- Contains a result with probability
+                    recipe["results"][idx]["amount_min"] = rspec["amount_min"] * scale
+                    recipe["results"][idx]["amount_max"] = rspec["amount_max"] * scale
+                end
+            end
+        end
+    end
+end
 
 local function generate_arc_smelting_recipe(original_recipe)
     if original_recipe["category"] ~= "smelting" then
         return
     end
 
-    recipe = {
-        ["type"] = "recipe",
-        ["name"] = "arc-" .. original_recipe["name"],
-        ["category"] = "arc-smelting",
-    }
+    recipe = utils_table.deep_copy(original_recipe)
 
-    copy_recipe_data(original_recipe, recipe, 10)
+    recipe["name"] = "arc-" .. original_recipe["name"]
+    recipe["category"] = "arc-smelting"
+    --Enable all recipes, since I do not know how to enable it after the specific research has been activated (like when enriched ores tech from Krastorio is unlocked)
+    recipe["enabled"] = true 
+
+    local scaling_factor = 10
+    scale_recipe(recipe, scaling_factor)
+
     data:extend({recipe})
     mimic_recipe_module_limitations(original_recipe["name"], recipe["name"])
 end
